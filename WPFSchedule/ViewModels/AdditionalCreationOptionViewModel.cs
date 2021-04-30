440c203346d2dc9b81358cb182ff7ebcd77e1efd
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-30 08:02:26 +0100

    Bug in namespaces was solved

--- WPFSchedule/ViewModels/AdditionalCreationOptionViewModel.cs
+++ WPFSchedule/ViewModels/AdditionalCreationOptionViewModel.cs
@@ -1,17 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+
 using WPFSchedule.Models;
 using WPFSchedule.Models.Commands;
-using WPFSchedule.Models.Enums;
+
+using WhatProject.Data.Enums;
 
 namespace WPFSchedule.ViewModels
 {
